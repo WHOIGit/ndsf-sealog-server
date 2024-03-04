5debd809433cde89cc03630ec2206048db417126
Mathew Schwartzman <mathew.schwartzman@whoi.edu>
2024-03-04 11:14:11 -0500

    fix: copies remote db files to local mongo (#28)
    * fix: copies remote db files to local mongo
    
    * fix: adds positional params
    
    ---------
    
    Co-authored-by: Mathew Schwartzman <mschwartzman@6river.com>

---
+++ dbload.sh
@@ -0,0 +1,34 @@
+echo "Welcome to Sealog dbload"
+echo "please input remote sealog server username"
+
+read username
+
+host="harmonyhill"  # sealog machine
+vehicle="jason" # vehicle we're using
+DEFAULT_CRUISE_NUMBER="TN406"  
+DEFAULT_DIVE_NUMBER="J2-1438"
+
+# If positional parameters are provided, override the default values
+CRUISE_NUMBER="${1:-$DEFAULT_CRUISE_NUMBER}"
+DIVE_NUMBER="${2:-$DEFAULT_DIVE_NUMBER}"
+dbdump_path="~/sealog-files/dbdump" # where would you like to stick this?
+
+cruise_file_name="$CRUISE_NUMBER"_cruiseRecord_mod.json
+
+echo "looking for cruise file $cruise_file_name"
+mkdir -p $dbdump_path/$CRUISE_NUMBER
+
+echo "rsyncing cruise from harmonyhill"
+rsync -r $username@$host:/home/sealog/Cruises/$CRUISE_NUMBER/modifiedForImport/$cruise_file_name $dbdump_path/$CRUISE_NUMBER/$cruise_file_name
+
+echo "rsyncing aux data, events, and lowerings"
+rsync -r $username@$host:/home/sealog/Cruises/$CRUISE_NUMBER/$DIVE_NUMBER/modifiedForImport/ $dbdump_path/$CRUISE_NUMBER/
+
+echo "importing into local mongo instance"
+mongoimport --db sealogDB --collection cruises --file $dbdump_path/$CRUISE_NUMBER/$cruise_file_name --mode upsert --jsonArray
+
+mongoimport --db sealogDB --collection lowerings --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_loweringRecord_mod.json --mode upsert --jsonArray
+mongoimport --db sealogDB --collection events --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_eventOnlyExport_mod.json --mode upsert --jsonArray
+mongoimport --db sealogDB --collection event_aux_data --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_auxDataExport_mod.json --mode upsert --jsonArray
+
+exit
