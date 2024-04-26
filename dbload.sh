a5a1aedee21bc51c3266b7aac7c0d1c9b7bef964
figuernd <nathan.figueroa@whoi.edu>
2024-04-26 13:21:40 -0400

    fix: fixes path bug and now detects whether import is object or array (#31)

--- dbload.sh
+++ dbload.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "Welcome to Sealog dbload"
 echo "please input remote sealog server username"
 
@@ -11,7 +13,7 @@ DEFAULT_DIVE_NUMBER="J2-1438"
 # If positional parameters are provided, override the default values
 CRUISE_NUMBER="${1:-$DEFAULT_CRUISE_NUMBER}"
 DIVE_NUMBER="${2:-$DEFAULT_DIVE_NUMBER}"
-dbdump_path="~/sealog-files/dbdump" # where would you like to stick this?
+dbdump_path="$HOME/sealog-files/dbdump" # where would you like to stick this?
 
 cruise_file_name="$CRUISE_NUMBER"_cruiseRecord_mod.json
 
@@ -24,11 +26,35 @@ rsync -r $username@$host:/home/sealog/Cruises/$CRUISE_NUMBER/modifiedForImport/$
 echo "rsyncing aux data, events, and lowerings"
 rsync -r $username@$host:/home/sealog/Cruises/$CRUISE_NUMBER/$DIVE_NUMBER/modifiedForImport/ $dbdump_path/$CRUISE_NUMBER/
 
-echo "importing into local mongo instance"
-mongoimport --db sealogDB --collection cruises --file $dbdump_path/$CRUISE_NUMBER/$cruise_file_name --mode upsert --jsonArray
 
-mongoimport --db sealogDB --collection lowerings --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_loweringRecord_mod.json --mode upsert --jsonArray
-mongoimport --db sealogDB --collection events --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_eventOnlyExport_mod.json --mode upsert --jsonArray
-mongoimport --db sealogDB --collection event_aux_data --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_auxDataExport_mod.json --mode upsert --jsonArray
+# Function to determine the JSON file type and return the appropriate jsonArray argument
+get_json_array_flag() {
+  local json_file=$1
+  local jsonArray=""
+
+  # Check if the JSON file contains an array
+  if head -n 1 "$json_file" | grep -q '^\['; then
+    # If it's an array, set the jsonArray variable to "--jsonArray"
+    jsonArray="--jsonArray"
+  fi
+
+  echo "$jsonArray"
+}
+
+echo "importing $dbdump_path/$CRUISE_NUMBER/$cruise_file_name"
+jsonArray=$(get_json_array_flag "$dbdump_path/$CRUISE_NUMBER/$cruise_file_name")
+mongoimport --db sealogDB --collection cruises --file $dbdump_path/$CRUISE_NUMBER/$cruise_file_name --mode upsert $jsonArray
+
+echo "importing $dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_loweringRecord_mod.json"
+jsonArray=$(get_json_array_flag "$dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_loweringRecord_mod.json")
+mongoimport --db sealogDB --collection lowerings --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_loweringRecord_mod.json --mode upsert $jsonArray
+
+echo "importing $dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_eventOnlyExport_mod.json"
+jsonArray=$(get_json_array_flag "$dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_eventOnlyExport_mod.json")
+mongoimport --db sealogDB --collection events --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_eventOnlyExport_mod.json --mode upsert $jsonArray
+
+echo "importing $dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_auxDataExport_mod.json"
+jsonArray=$(get_json_array_flag "$dbdump_path/$CRUISE_NUMBER/$DIVE_NUMBER""_auxDataExport_mod.json")
+mongoimport --db sealogDB --collection event_aux_data --file $dbdump_path/$CRUISE_NUMBER/"$DIVE_NUMBER"_auxDataExport_mod.json --mode upsert $jsonArray
 
 exit
